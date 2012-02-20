cc310e9c0cf2619db89207bdfd4e3d0bcfa39d22
Theodore R. Smith <theodore@phpexperts.pro>
2012-02-20 12:58:29 -0600

    Removed RedditMirror-specific code.

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
 require 'WebPageConsolidator.inc.php';
-require_once dirname(__FILE__) . '/../.dbcreds';
 
 // Command line stuff here
 if (php_sapi_name() == 'cli')
@@ -29,8 +27,6 @@ if (php_sapi_name() == 'cli')
     }
 }
 
-//print_r($_GET); exit;
-
 if (!isset($_GET['base']))
 {
 	echo "ERROR: A directory to parse is required.\n";
@@ -43,24 +39,13 @@ $orig_basedir = $basedir = (isset($argv[1])) ?
 //echo "Basedir: $orig_basedir";
 if (!file_exists($basedir))
 {
-	if (file_exists('../cache/websites/' . $basedir))
+	if (!mkdir($basedir))
 	{
-		$basedir = '../cache/websites/' . $basedir;
-	}
-	else
-	{
-		throw new RuntimeException('Cannot find basedir: ' . htmlentities($basedir));
+		throw new RuntimeException('Cannot find or create the basedir: ' . $basedir);
 	}
 }
 
-$redditKey = substr($orig_basedir, strrpos($orig_basedir, '_') + 1);
-
-$pdo = new PDO('mysql:host=' . DBConfig::$host . ';dbname=' . DBConfig::$db, DBConfig::$user, DBConfig::$pass);
-$stmt = $pdo->prepare('SELECT url FROM vw_RedditLinks WHERE redditKey=?');
-$stmt->execute(array($redditKey));
-$originalURL = $stmt->fetchColumn();
-
-$outputDir = isset($argv[2]) ? $argv[2] : '../cache/consolidated/' . $orig_basedir;
+$outputDir = isset($argv[2]) ? $argv[2] : $orig_basedir;
 
 $consolidator = new WebpageConsolidator;
 $preHTML = '<h3 style="text-align: center; padding-bottom: 5px; margin-bottom: 5px; border-bottom: black 2px solid; width: 100%; height: 1em; background: white; text-transform: none">ORIGINAL URL: <a href="' . htmlspecialchars($originalURL) . '">' . htmlspecialchars($originalURL) . '</a></h3>';
@@ -68,16 +53,6 @@ $html = $consolidator->consolidate(array('basedir' => $basedir,
 			                             'outputDir' => $outputDir,
 										 'preHTML' => $preHTML));
 
-if ($html == WebpageConsolidator::STATUS_BLANK_HTML)
-{
-	// Redirect to cache...
-//	header('HTTP/1.1 301 Moved Permanently');
-//	header('Location: http://' . $_SERVER['HTTP_HOST'] . '/cache/websites/' . $orig_basedir . '/');
-	echo "<div style=\"text-align: center\"><h3>ORIGINAL URL: <a href=\"$originalURL\">$originalURL</a></h3>\n";
-	$url = 'http://' . $_SERVER['HTTP_HOST'] . '/cache/websites/' . $orig_basedir . '/' . preg_replace('/http:\/\//', '', $originalURL);
-    printf('<img src="%s" alt="%s"/></div>', $url, $originalURL); 
-	exit;
-}
 
 if (isset($_GET['delete']))
 {
